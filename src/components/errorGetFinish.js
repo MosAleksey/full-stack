be768a1a0a5149c2339ab7b5499d8f5a33c1b5be
Aleksey Mosin <mos.aleksey@gmail.com>
2022-05-19 02:33:37 +0300

    19.05.2022

--- src/components/errorGetFinish.js
+++ src/components/errorGetFinish.js
@@ -1,14 +1,76 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {Button, Col, Form, Row, Table} from "react-bootstrap";
 import {observer} from "mobx-react-lite";
 import {Context} from "../index";
 import {user_query} from "./queries/user_query";
+import {error__inspect_query, error__inwork__query} from "./queries/error__query";
+import {store__query__bybrand} from "./queries/store_query";
+import ErrorGetFinishFooter from "./errorGetFinishFooter";
 
-const ErrorGetFinish = observer(() => {
+const ErrorGetFinish = observer(({fields, status}) => {
     const {__user} = useContext(Context)
+    const {__error} = useContext(Context)
+    const [userWorks, setUserWorks] = useState([])
+    const [storeByBrand, setStoreByBrand] = useState([])
+    const [userInspect, setUserInspect] = useState([])
+    const [errorInspect, setErrorInspect] = useState([])
+    const [checkUser, setCheckUser] = useState([])
+    const [repairKitId, setRepairKitId] = useState([])
+    const [getFinish, setGetFinish] = useState({
+        descript_finish_work:'',
+        user_work_id:'',
+        repair_kit_id:'',
+        user_inspect_id:'',
+        descript_inspect_error:'',
+        finish_date:'',
+        geometry_fail_akt:'',
+        geometry_finish_akt: null
+    })
+
+    const filter_userId = (user_id) => {
+        let user = []
+        JSON.parse(user_id).forEach(arr => {
+            __user.data_User.forEach(element => {
+                if (String(arr) === String(element.id))
+                    user.push(element)
+            })
+        })
+        return (user)
+    }
+
+    const fettle = (fettle) => {
+        if (fettle === 1)
+            return 'Новая'
+        if (fettle === 2)
+            return 'Б/У'
+    }
+
     useEffect(()=>{
+        error__inwork__query(fields.id).then(data => __error.setInWorkError(data)).finally(()=> setUserWorks(filter_userId(__error.data_InWork_Error.user_id)))
+        error__inspect_query(fields.id).then(data => setUserInspect(filter_userId(data[0].user_id))).finally(() => setErrorInspect(__error.data_Inspect_Error[0]))
         user_query().then(data => __user.setUser(data))
+        store__query__bybrand(fields.b_name).then(data=> setStoreByBrand(data))
     }, [])
+
+    const addFinishError = () => {
+        const formData = new FormData()
+        formData.append('user_finish_id', JSON.stringify(checkUser))
+        formData.append('descript_finish_work', getFinish.descript_finish_work)
+        formData.append('user_work_id', __error.data_InWork_Error.user_id)
+        formData.append('repair_kit_id', JSON.stringify(repairKitId))
+        formData.append('user_inspect_id', __error.data_Inspect_Error[0].user_id)
+        formData.append('descript_inspect_error', errorInspect.inspect_error)
+        formData.append('finish_date', Date().toLocaleString())
+        formData.append('geometry_fail_akt', errorInspect.geometry_fail_akt)
+        formData.append('geometry_finish_akt', getFinish.geometry_finish_akt)
+        console.log(formData.get('repair_kit_id'))
+    }
+
+    const selectFile = e =>
+    {
+        setGetFinish({...getFinish, geometry_finish_akt: e.target.files[0]})
+    }
+
     return (
         <div className="mt-2" style={{border: "2px solid black", borderRadius: "10px", padding: '20px'}}>
             <Form>
@@ -17,7 +79,7 @@ const ErrorGetFinish = observer(() => {
                         <Col className="col-lg-6">
                             <div>
                                 <Form.Text>
-                                    <h5>Комиссия к осмотру</h5><p>В комиссии к осмотру состоят только сотрудники
+                                    <h5>Комиссия к осмотру (Сдача оборудования в работу)</h5><p>В комиссии к осмотру состоят только сотрудники
                                     имеющие специальность "Сервисный инженер" или "Механик"</p>
                                 </Form.Text>
                             </div>
@@ -48,7 +110,9 @@ const ErrorGetFinish = observer(() => {
                                                 <td>{element.second_name} {element.first_name}</td>
                                                 <td>{element.function_title}</td>
                                                 <td><Form.Check
-                                                    type="checkbox"/></td>
+                                                    type="checkbox"
+                                                    onChange={(e)=> setCheckUser([...checkUser, element.id])}
+                                                /></td>
                                             </tr>
                                     }
                                 )}
@@ -58,7 +122,7 @@ const ErrorGetFinish = observer(() => {
                         <Col className="col-lg-6">
                             <div>
                                 <div>
-                                    <Form.Control as="textarea" placeholder="Описание"/>
+                                    <Form.Control onChange={(e) => setGetFinish({...getFinish, descript_finish_work: e.target.value})} as="textarea" placeholder="Описание"/>
                                 </div>
                             </div>
                         </Col>
@@ -76,11 +140,16 @@ const ErrorGetFinish = observer(() => {
                                     <th>Табельный номер</th>
                                     <th>ФИО</th>
                                     <th>Должность</th>
-                                    <th>Выбрать</th>
                                 </tr>
                                 </thead>
                                 <tbody>
-
+                                {userWorks.map(element =>
+                                    <tr key={element.id}>
+                                        <td>{element.personal_number}</td>
+                                        <td>{element.second_name} {element.first_name}</td>
+                                        <td>{element.function_title}</td>
+                                    </tr>
+                                )}
                                 </tbody>
                             </Table>
                         </Col>
@@ -93,22 +162,42 @@ const ErrorGetFinish = observer(() => {
                             <Table striped bordered size="sm">
                                 <thead>
                                 <tr>
-                                    <th>Запчасти</th>
-                                    <th>Запчасти</th>
-                                    <th>Запчасти</th>
-                                    <th>Запчасти</th>
+                                    <th>Оборудование</th>
+                                    <th>Наименование</th>
+                                    <th>Состояние</th>
+                                    <th>Наличие</th>
+                                    <th>Использовано</th>
                                 </tr>
                                 </thead>
                                 <tbody>
-
+                                    {storeByBrand.map(element=>
+                                    <tr key={element.id}>
+                                        <td>{element.m_title}</td>
+                                        <td>{element.title}</td>
+                                        <td>{fettle(element.fettle)}</td>
+                                        <td>{element.availability}</td>
+                                        <td><Form.Control defaultValue="0" onChange={(e) => {setRepairKitId([...repairKitId, element.id])}}/></td>
+                                    </tr>
+                                    )}
                                 </tbody>
                             </Table>
                         </Col>
                     </Row>
+                    <Row>
+                        <Col className="col-lg-6">
+                            <Form.Text><h5>Геометрическая точность</h5></Form.Text>
+                            <Form.Text><p>Загрузите заполненый акт после ремонта</p></Form.Text>
+                            <Form.Control type="file" onChange={selectFile}/>
+                        </Col>
+                    </Row>
+                        {/*<ErrorGetFinishFooter userInspect={userInspect} errorInspect={errorInspect}/>*/}
                     <hr/>
                     <Row>
                         <Col className="col-lg-6">
                             <Button disabled variant="success">Завершить</Button>
+                        </Col>
+                        <Col className="col-lg-6">
+                            <Button onClick={()=> addFinishError()}>Test</Button>
                         </Col>
                     </Row>
                 </Form.Group>
---
+++ src/components/errorGetFinishFooter.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {Button, Col, Form, Row, Table} from "react-bootstrap";
+
+const ErrorGetFinishFooter = ({userInspect, errorInspect}) => {
+    return (
+        <div>
+            <hr/>
+            <Row>
+                <Col className="col-lg-6 mb-3">
+                    <div>
+                        <Form.Text><h5>Данные для формирования отчета</h5></Form.Text>
+                    </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="col-lg-6">
+                    <div>
+                        <Form.Text><h5>Комиссия выполнявшая дефектовку</h5><p>* Нередактируемое поле</p></Form.Text>
+                    </div>
+                    <div>
+                        <Table striped bordered size="sm">
+                            <thead>
+                            <tr>
+                                <th>Табельный номер</th>
+                                <th>ФИО</th>
+                                <th>Должность</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {userInspect.map(element =>
+                                <tr key={element.id}>
+                                    <td>{element.personal_number}</td>
+                                    <td>{element.second_name} {element.first_name}</td>
+                                    <td>{element.function_title}</td>
+                                </tr>
+                            )}
+                            </tbody>
+                        </Table>
+                    </div>
+                </Col>
+                <Col className="col-lg-6">
+                    <div>
+                        <Form.Text><h5>Выявленные комиссией неисправности</h5><p> </p></Form.Text>
+                    </div>
+                    <div>
+                        <Form.Control defaultValue={errorInspect.inspect_error} as="textarea" placeholder="Выявленные неисправности"/>
+                    </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="col-lg-6">
+                    <div className="mb-2 mt-2">
+                        <Form.Text>Акт проверки оборудования на геометрическую точность до начала ремонта</Form.Text>
+                    </div>
+                    <a href={`http://localhost:8080/${errorInspect.geometry_fail_akt}`} target="_blank">Скачать акт</a>
+                </Col>
+            </Row>
+        </div>
+    );
+};
+
+export default ErrorGetFinishFooter;
